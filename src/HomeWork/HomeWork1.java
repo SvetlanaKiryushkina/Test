a4340c7fab4055ba88af927eb4c006364949fb35
SvetlanaKiryushkina <svetlana198787@mail.ru>
2026-05-03 11:25:18 +0300

    two commit

--- src/HomeWork/HomeWork1.java
+++ src/HomeWork/HomeWork1.java
@@ -2,6 +2,6 @@ package HomeWork;
 
 public class HomeWork1 {
     public static void main (String [] args){
-        System.out.println("ФИО Разработчика. 22.04.2016");
+        System.out.println("ФИО Разработчика, 22.04.2016");
     }
 }
